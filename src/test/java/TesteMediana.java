c2fc5d4647c6af6c508bab6ff7908d4bd49f44ad
Lua machado <lua_ivo@hotmail.com>
2018-03-28 19:34:39 -0300

    teste tdd vetor vazio

--- src/test/java/TesteMediana.java
+++ src/test/java/TesteMediana.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import com.mycompany.mavenproject1.Parte2.Mediana;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -26,6 +27,17 @@ public class TesteMediana {
             fail();
         } catch (Exception ex) {
             assertEquals("Erro vetor e nulo", ex.getMessage());
+        }
+     }
+    @Test
+     public void testvetorVazio() {
+        double vetor[]= {};
+        Mediana mediana = new Mediana();
+        try {
+            double res = mediana.acharMediana(vetor);
+            fail();
+        } catch (Exception ex) {
+            assertEquals("Erro vetor e vazio", ex.getMessage());
         }
      }
 }
